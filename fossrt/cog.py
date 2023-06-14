cf4243a4732bfdce5675d95bb3f22036397b78aa
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2023-06-14 18:22:09 -0500

    Add microstrain conversion

--- fossrt/cog.py
+++ fossrt/cog.py
@@ -4,3 +4,6 @@ helper functions for decoding CoG data
 
 def cog_to_wavelength(binary):
     return 1514+((int(binary, 2) * (1586-1514))/(2 ** 18))
+
+def cog_to_strain(binary, initial_wavelength, strain_factor=0.78):
+    return (((int(binary, 2) - initial_wavelength)/(initial_wavelength*strain_factor)) * (10 ** 6))
