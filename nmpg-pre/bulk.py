64ee3e4adee6b3c8858a104bbcac54ce8355d51a
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2023-08-09 14:00:18 -0500

    Bulk preprocessing

---
+++ nmpg-pre/bulk.py
@@ -0,0 +1,26 @@
+import argparse
+from betterlib.logging import Logger
+import os
+from tqdm import tqdm
+import json
+import sys
+
+logger = Logger("./nmpg-pre-bulk.log", "NMPG Preprocessor (Bulk Mode)")
+
+debug = True
+
+
+parser = argparse.ArgumentParser(description='Data pre-processing script for NMPG/MPG-FOSS')
+parser.add_argument('data', metavar='data', type=str, help='Path to data (standard format: microstrain dir and weight_data.csv)')
+parser.add_argument('--indent', dest='indent', action='store_true', help='Export JSON with indentation')
+args = parser.parse_args()
+logger.debug("parsed args")
+
+dirs = os.listdir(args.data)
+dirs.sort()
+
+for i in tqdm(range(len(dirs)), desc="Preprocessing data...", unit="drains"):
+    os.system("python3 nmpg-pre/nmpg-pre.py " + os.path.join(args.data, dirs[i]) + " --no-interact " + ("--indent" if args.indent else ""))
+    logger.info("Preprocessed drain " + str(i+1) + "/" + str(len(dirs)) + " (" + dirs[i] + ")")
+
+logger.info("Preprocessing complete.")
